bc1ae3fca31e41bf98a048222e864d50093e826b
Nick Hecht <chilliwebs@gmail.com>
2019-08-07 13:27:06 -0400

    test
    test

--- configure.groovy
+++ configure.groovy
@@ -1,13 +1,11 @@
-import jenkins.model.Jenkins
-import org.jenkins.plugins.lockableresources.LockableResource
-import org.jenkins.plugins.lockableresources.LockableResourcesManager
-
-
 //--COMMON LIBS--
 this.vm_mgmt_common = evaluate dir('/var/jenkins_home/workspace/Vmware_Managment_Pipeline_master') { return readFile('common.groovy')  }
 this.acro_mgmt_common = evaluate dir('/var/jenkins_home/workspace/crohub_Managment_Pipeline_master') { return readFile('common.groovy')  }
 this.machines_json = dir('/var/jenkins_home/workspace/Vmware_Managment_Pipeline_masterconf')  { return readFile('machines.json') }
 this.devices_json = dir('/var/jenkins_home/workspace/crohub_Managment_Pipeline_masterconf')  { return readFile('devices.json') }
+def restore_snapshot(vmx, snapshot){return this.vm_mgmt_common.restore_snapshot(vmx, snapshot)}
+def start_vm(vmx){return this.vm_mgmt_common.start_vm(vmx)}
+
 this.common = evaluate readFile('common.groovy')
 def test(){return this.common.test()}
 
@@ -18,9 +16,12 @@ def get_device_info(devid){return (new groovy.json.JsonSlurper()).parseText(this
 echo('trying to lock a Windows10 resource')
 lock(label:'Windows10', quantity: 1, variable:'vmid') {
     echo('using vm: '+env.vmid)
+    def machine = get_machine_info(env.vmid)
     lock(label:'Levi', quantity: 1, variable:'devid') {
         echo('using device: '+env.devid)
-        echo(get_machine_info(env.vmid).toString())
-        echo(get_device_info(env.devid).toString())
+        echo(jenkins.model.jenkins.getInstance().getComputer(machine.name).getJnlpMac())
+        //restore_snapshot(machine.vmxurl, machine.snapshot)
+        //start_vm(machine.vmxurl)
+        //sudo vmrun -gu vmuser -gp password runScriptInGuest /home/vmuser/vmware/W10HS_1/W10HS_1.vmx "" "powershell -Command \"Invoke-WebRequest http://172.17.0.1:8080/jnlpJars/agent.jar -OutFile C:\\Users\\vmuser\\Desktop\\agent.jar\""
     }
 }
