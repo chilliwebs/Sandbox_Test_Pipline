f407a463a6cef11069b0715091aae35e5a1761ad
Nick Hecht <chilliwebs@gmail.com>
2019-08-08 09:55:35 -0400

    test
    test

--- common.groovy
+++
@@ -1,5 +0,0 @@
-def test() {
-    echo('test')
-}
-
-return this
---
+++ vm_exec.groovy
@@ -0,0 +1,13 @@
+//--COMMON LIBS--
+// def vm_mgmt_common = evaluate dir('/var/jenkins_home/workspace/Vmware_Managment_Pipeline_master') { return readFile('common.groovy')  }
+// def machines_json = dir('/var/jenkins_home/workspace/Vmware_Managment_Pipeline_masterconf')  { return readFile('machines.json') }
+//---------------
+
+def vm_exec() {
+    echo("Testing")
+    echo('were in!')
+    sleep(30)
+    bat('shutdown /s /f /t 0')
+}
+
+vm_exec()
