e283b8ab75b685b94fe202664398ca3298e1fb56
Nick Hecht <chilliwebs@gmail.com>
2019-08-07 22:54:30 -0400

    fixing node name
    fixing node name

--- configure.groovy
+++ configure.groovy
@@ -25,7 +25,7 @@ def configure() {
                 def vmIP = get_vm_ipaddr(machine.vmxurl)
                 def masterIP = InetAddress.localHost.hostAddress
                 run_script_on_vm('vmuser', 'password', machine.vmxurl, "", "powershell -Command \"Invoke-WebRequest http://${masterIP}:8080/jnlpJars/agent.jar -OutFile C:\\Users\\vmuser\\Desktop\\agent.jar\"")
-                run_script_on_vm('vmuser', 'password', machine.vmxurl, "", "java -Dhudson.util.ProcessTree.disable=true -jar \"C:\\Users\\vmuser\\Desktop\\agent.jar\" -jnlpUrl http://${masterIP}:8080/computer/W10HS_1/slave-agent.jnlp -secret 9047c64d32bfa07e89bf14245bcb6a1023ee4f6942f4a5b597d6b7a244bf9b3f -workDir \"C:\\User\\vmuser\"", false)
+                run_script_on_vm('vmuser', 'password', machine.vmxurl, "", "java -Dhudson.util.ProcessTree.disable=true -jar \"C:\\Users\\vmuser\\Desktop\\agent.jar\" -jnlpUrl http://${masterIP}:8080/computer/${machine.name}/slave-agent.jnlp -secret 9047c64d32bfa07e89bf14245bcb6a1023ee4f6942f4a5b597d6b7a244bf9b3f -workDir \"C:\\User\\vmuser\"", false)
             }
         }
         stage('vm execution') {
