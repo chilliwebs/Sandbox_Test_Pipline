c38cd237187d672afc117d0cb332ee6e809acefa
Nick Hecht <chilliwebs@gmail.com>
2019-08-07 22:27:17 -0400

    test
    test

--- configure.groovy
+++ configure.groovy
@@ -15,19 +15,34 @@ def test(){return this.common.test()}
 def configure() {
     lock(label:'Windows10', quantity: 1, variable:'vmid') {
         def machine = new HashMap<>((new groovy.json.JsonSlurper()).parseText(this.machines_json)[env.vmid])
-        lock(label:'Levi', quantity: 1, variable:'devid') {
-            def device = new HashMap<>((new groovy.json.JsonSlurper()).parseText(this.devices_json)[env.devid])
-            restore_snapshot(machine.vmxurl, machine.snapshot)
-            start_vm(machine.vmxurl)
-            def vmIP = get_vm_ipaddr(machine.vmxurl)
-            def masterIP = InetAddress.localHost.hostAddress
-            run_script_on_vm('vmuser', 'password', machine.vmxurl, "", "powershell -Command \"Invoke-WebRequest http://${masterIP}:8080/jnlpJars/agent.jar -OutFile C:\\Users\\vmuser\\Desktop\\agent.jar\"")
-            run_script_on_vm('vmuser', 'password', machine.vmxurl, "", "java -Dhudson.util.ProcessTree.disable=true -jar \"C:\\Users\\vmuser\\Desktop\\agent.jar\" -jnlpUrl http://${masterIP}:8080/computer/W10HS_1/slave-agent.jnlp -secret 9047c64d32bfa07e89bf14245bcb6a1023ee4f6942f4a5b597d6b7a244bf9b3f -workDir \"C:\\User\\vmuser\"", false)
-            node(machine.name) {
-                echo('were in!')
-                bat 'shutdown /s /f /t 0'
+        //lock(label:'Levi', quantity: 1, variable:'devid') {
+        //    def device = new HashMap<>((new groovy.json.JsonSlurper()).parseText(this.devices_json)[env.devid])
+        stage('setup vm') {
+            agent {
+                label("master")
+            }
+            steps {
+                echo("Building")
+                restore_snapshot(machine.vmxurl, machine.snapshot)
+                start_vm(machine.vmxurl)
+                def vmIP = get_vm_ipaddr(machine.vmxurl)
+                def masterIP = InetAddress.localHost.hostAddress
+                run_script_on_vm('vmuser', 'password', machine.vmxurl, "", "powershell -Command \"Invoke-WebRequest http://${masterIP}:8080/jnlpJars/agent.jar -OutFile C:\\Users\\vmuser\\Desktop\\agent.jar\"")
+                run_script_on_vm('vmuser', 'password', machine.vmxurl, "", "java -Dhudson.util.ProcessTree.disable=true -jar \"C:\\Users\\vmuser\\Desktop\\agent.jar\" -jnlpUrl http://${masterIP}:8080/computer/W10HS_1/slave-agent.jnlp -secret 9047c64d32bfa07e89bf14245bcb6a1023ee4f6942f4a5b597d6b7a244bf9b3f -workDir \"C:\\User\\vmuser\"", false)
             }
         }
+        stage('vm execution') {
+            agent {
+                label(machine.name)
+            }
+            steps {
+                echo("Testing")
+                echo('were in!')
+                sleep(30)
+                bat('shutdown /s /f /t 0')
+            }
+        }
+        //}
     }
 }
 configure()
