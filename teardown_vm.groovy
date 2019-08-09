e577017d055b0e519bd7d977f4a44d55a19f81b6
Nick Hecht <chilliwebs@gmail.com>
2019-08-08 22:56:59 -0400

    test
    test

--- teardown_vm.groovy
+++ teardown_vm.groovy
@@ -6,7 +6,7 @@ this.machines_json = dir('/var/jenkins_home/workspace/Vmware_Managment_Pipeline_
 def teardown_vm() {
     machine = new HashMap<>((new groovy.json.JsonSlurper()).parseText(this.machines_json)[env.vmid])
     echo("tearing down")
-    Jenkins.instance.getNode(machine.name).getComputer().disconnect(hudson.slaves.OfflineCause.create(hudson.slaves.Messages._RetentionStrategy_Demand_OfflineIdle))
+    Jenkins.instance.getNode(machine.name).getComputer().disconnect(hudson.slaves.OfflineCause.create(hudson.slaves.Messages._RetentionStrategy_Demand_OfflineIdle()))
     this.vmgmt.stop_vm(machine.vmxurl)
 }
 
