ff46855da78d97e4c5e87302a62ece740949f057
Nick Hecht <nh49741@bose.com>
2019-09-09 14:10:50 -0400

    cleanup
    cleanup

--- configure.groovy
+++ configure.groovy
@@ -1,7 +1,7 @@
 def configure() {
     echo('configure')
-    sh "wget https://downloads.bose.com/ced/boseupdater/windows/BoseUpdaterInstaller_6.0.0.4388.exe"
-    stash "BoseUpdaterInstaller_6.0.0.4388.exe"
+    //sh "wget https://downloads.bose.com/ced/boseupdater/windows/BoseUpdaterInstaller_6.0.0.4388.exe"
+    //stash "BoseUpdaterInstaller_6.0.0.4388.exe"
 }
 
 configure()
