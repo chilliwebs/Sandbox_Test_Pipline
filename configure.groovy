87b6cee0e89913c5891a6cf096f713285e914c12
Nick Hecht <nh49741@bose.com>
2019-09-09 11:26:12 -0400

    removing maven zip
    removing maven zip

--- configure.groovy
+++ configure.groovy
@@ -1,7 +1,5 @@
 def configure() {
     echo('configure')
-    sh "wget https://www-us.apache.org/dist/maven/maven-3/3.6.2/binaries/apache-maven-3.6.2-bin.zip"
-    stash "apache-maven-3.6.2-bin.zip"
     stash "chromedriver.exe"
     stash "geckodriver.exe"
     stash "IEDriverServer.exe"
