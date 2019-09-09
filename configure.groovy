aa63361434a2be6bba0e6dfb39df2f6e1a581cf0
Nick Hecht <nh49741@bose.com>
2019-09-09 10:36:12 -0400

    testing building outside of vm
    testing building outside of vm

--- configure.groovy
+++ configure.groovy
@@ -6,6 +6,30 @@ def configure() {
     stash "geckodriver.exe"
     stash "IEDriverServer.exe"
     stash "MicrosoftWebDriver.exe"
+
+    sh "mvn clean install"
+    sh "mvn dependency:copy-dependencies"
+
+    dir('target') {
+        stash "Sandbox_Test_Pipline-1.0-SNAPSHOT-tests.jar"
+        dir('dependency') {
+            stash "guava-25.0-jre.jar"
+            stash "hamcrest-core-1.3.jar"
+            stash "junit-4.11.jar"
+            stash "okhttp-3.11.0.jar"
+            stash "okio-1.14.0.jar"
+            stash "selenium-api-3.141.59.jar"
+            stash "selenium-remote-driver-3.141.59.jar"
+            stash "selenium-support-3.141.59.jar"
+
+            stash "selenium-chrome-driver-3.141.59.jar"
+            stash "selenium-edge-driver-3.141.59.jar"
+            stash "selenium-firefox-driver-3.141.59.jar"
+            stash "selenium-ie-driver-3.141.59.jar"
+            stash "selenium-opera-driver-3.141.59.jar"
+            stash "selenium-safari-driver-3.141.59.jar"
+        }
+    }
 }
 
 configure()
--- vm_exec.groovy
+++ vm_exec.groovy
@@ -9,19 +9,46 @@ def vm_exec() {
     echo("Testing")
     echo('were in!')
 
-    unstash "chromedriver.exe"
-    unstash "geckodriver.exe"
-    unstash "IEDriverServer.exe"
-    unstash "MicrosoftWebDriver.exe"
+    unstash "Sandbox_Test_Pipline-1.0-SNAPSHOT-tests.jar"
+    unstash "guava-25.0-jre.jar"
+    unstash "hamcrest-core-1.3.jar"
+    unstash "junit-4.11.jar"
+    unstash "okhttp-3.11.0.jar"
+    unstash "okio-1.14.0.jar"
+    unstash "selenium-api-3.141.59.jar"
+    unstash "selenium-remote-driver-3.141.59.jar"
+    unstash "selenium-support-3.141.59.jar"
 
-    unstash "apache-maven-3.6.2-bin.zip"
-    unzip zipFile: "apache-maven-3.6.2-bin.zip", dir: "C:\\maven"
-
-    withEnv(["PATH+MAVEN=C:\\maven\\apache-maven-3.6.2\\bin"]) {
-        bat 'set'
-        bat "dir"
-        bat "mvn clean test -Dbrowser=\"${env.browser}\""
+    if(env.browser == "chrome") {
+        unstash "chromedriver.exe"
+        unstash "selenium-chrome-driver-3.141.59.jar"
     }
+    if(env.browser == "firefox") {
+        unstash "geckodriver.exe"
+        unstash "selenium-firefox-driver-3.141.59.jar"
+    }
+    if(env.browser == "internet explorer") {
+        unstash "IEDriverServer.exe"
+        unstash "selenium-ie-driver-3.141.59.jar"
+    }
+    if(env.browser == "MicrosoftEdge") {
+        unstash "MicrosoftWebDriver.exe"
+        unstash "selenium-edge-driver-3.141.59.jar"
+    }
+
+    // unstash "selenium-opera-driver-3.141.59.jar"
+    // unstash "selenium-safari-driver-3.141.59.jar"
+
+    // unstash "apache-maven-3.6.2-bin.zip"
+    // unzip zipFile: "apache-maven-3.6.2-bin.zip", dir: "C:\\maven"
+
+    // withEnv(["PATH+MAVEN=C:\\maven\\apache-maven-3.6.2\\bin"]) {
+    //     bat 'set'
+    //     bat "dir"
+    //     bat "mvn clean test -Dbrowser=\"${env.browser}\""
+    // }
+
+    bat "java -cp * -Dbrowser=\"${env.browser}\" org.junit.runner.JUnitCore com.chilliwebs.Sandbox_Test_Pipline.SimpleFWUpdateTest"
 
     echo('done!')
     bat 'shutdown /s /f /t 0'
