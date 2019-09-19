db52823824b83bf832f36c23b0b04f879e110af5
Nick Hecht <nh49741@bose.com>
2019-09-19 15:58:38 -0400

    moving work
    moving work

--- setup_vm.groovy
+++ setup_vm.groovy
@@ -1,5 +1,3 @@
-import groovy.json.JsonOutput
-
 //--COMMON LIBS--
 this.vmware = evaluate 'http://172.17.0.1:8765/api.groovy'.toURL().text
 this.acro = evaluate 'http://172.17.0.1:9876/api.groovy'.toURL().text
@@ -20,23 +18,12 @@ def setup_vm() {
         this.vmware.setVMProperty(machine.vmxurl, "usb_xhci.autoConnect.device${idx}", "path:${path.replaceAll(/[-\.]/,"/")} autoclean:1")
     })
     this.vmware.startVM(machine.vmxurl)
-    this.acro.setPortInfo(env.dev.split('-')[0], (env.dev.split('-')[1]).toInteger(), 'ON')
 
-    // this enssure the vm is ready
-    def vmIP = this.vmware.getIpAddr(machine.vmxurl)
-    echo("vm ip addr: ${vmIP}")
-
-    def masterIP = InetAddress.localHost.hostAddress
+    def masterIP = "172.17.0.1" //InetAddress.localHost.hostAddress
     def secret = jenkins.model.Jenkins.getInstance().getComputer(env.vmnod).getJnlpMac()
 
     this.vmware.runScriptOnVM(machine.vmxurl,'vmuser', 'password', "", 
         "schtasks /create /tn \"shutdown timeout\" /tr \"shutdown.exe /s /f /t 0\" /sc onidle /i 30")
-    this.vmware.runScriptOnVM(machine.vmxurl,'vmuser', 'password', "", 
-        "powershell -Command \"Invoke-WebRequest https://downloads.bose.com/ced/boseupdater/windows/BoseUpdaterInstaller_6.0.0.4388.exe -OutFile C:\\Users\\vmuser\\Desktop\\BoseUpdaterInstaller_6.0.0.4388.exe\"")
-    this.vmware.runScriptOnVM(machine.vmxurl,'vmuser', 'password', "",
-        "powershell -Command \"Start-Process C:\\Users\\vmuser\\Desktop\\BoseUpdaterInstaller_6.0.0.4388.exe\" -verb RunAs", false, true)
-    sleep(10)
-    this.vmware.sendKeysToVM(machine.vmxurl, "left enter")
     this.vmware.runScriptOnVM(machine.vmxurl,'vmuser', 'password', "", 
         "powershell -Command \"Invoke-WebRequest http://${masterIP}:8080/jnlpJars/agent.jar -OutFile C:\\Users\\vmuser\\Desktop\\agent.jar\"")
     this.vmware.runScriptOnVM(machine.vmxurl,'vmuser', 'password', "", 
--- vm_exec.groovy
+++ vm_exec.groovy
@@ -1,6 +1,12 @@
 
+//--COMMON LIBS--
+this.vmware = evaluate 'http://172.17.0.1:8765/api.groovy'.toURL().text
+this.acro = evaluate 'http://172.17.0.1:9876/api.groovy'.toURL().text
+//---------------
+
 def vm_exec() {
     echo "**GOT VM ${env.vmid}**"
+    echo "**GOT Device ${env.dev}**"
     echo "**GOT NODE ${env.vmnod}**"
     echo("Testing")
     echo('were in!')
@@ -31,6 +37,15 @@ def vm_exec() {
         unstash "MicrosoftWebDriver.exe"
         unstash "selenium-edge-driver-3.141.59.jar"
     }
+
+    this.acro.setPortInfo(env.dev.split('-')[0], (env.dev.split('-')[1]).toInteger(), 'ON')
+
+    this.vmware.runScriptOnVM(machine.vmxurl,'vmuser', 'password', "", 
+        "powershell -Command \"Invoke-WebRequest https://downloads.bose.com/ced/boseupdater/windows/BoseUpdaterInstaller_6.0.0.4388.exe -OutFile C:\\Users\\vmuser\\Desktop\\BoseUpdaterInstaller_6.0.0.4388.exe\"")
+    this.vmware.runScriptOnVM(machine.vmxurl,'vmuser', 'password', "",
+        "powershell -Command \"Start-Process C:\\Users\\vmuser\\Desktop\\BoseUpdaterInstaller_6.0.0.4388.exe\" -verb RunAs", false, true)
+    sleep(10)
+    this.vmware.sendKeysToVM(machine.vmxurl, "left enter")
 
     bat "java -cp * -Dbrowser=\"${env.browser}\" org.junit.runner.JUnitCore com.chilliwebs.Sandbox_Test_Pipline.SimpleFWUpdateTest"
 
