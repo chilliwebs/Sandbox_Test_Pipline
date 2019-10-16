b9bd711ae18201209b01d5d534000ede49f7df39
Nick Hecht <nh49741@bose.com>
2019-10-16 11:24:36 -0400

    Update results.js

--- results.js
+++ results.js
@@ -5,16 +5,14 @@ function loadReport() {
     });
 }
 
-console.log('loading JQuery...');
 
 //Load jQuery library using plain JavaScript
-(function () {
-    var newscript = document.createElement('script');
-    newscript.type = 'text/javascript';
-    newscript.async = true;
-    newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
-    newscript.onload = function () {
-        loadReport();
-    };
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(newscript);
-})();
+console.log('loading JQuery...');
+var newscript = document.createElement('script');
+newscript.type = 'text/javascript';
+newscript.async = true;
+newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
+newscript.onload = function () {
+    loadReport();
+};
+(document.getElementsByTagName('body')[0]).appendChild(newscript);
